dec0301f00f490613494ddfd5e0dc47614ce09ed
torsku <torsku@stud.ntnu.no>
2018-10-04 21:53:24 +0200

    changed back to expo-camera as other third party libraries don't work with expo, added icons #2 #3

---
+++ components/Cam.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {Text,View,TouchableOpacity,StyleSheet} from 'react-native';
+import{Container,Content,Header,Item,Icon,Input,Button} from 'native-base';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Camera, Permissions } from 'expo';
+
+export default class Cam extends React.Component {
+    state = {
+        hasCameraPermission: null,
+        type: Camera.Constants.Type.back
+    }
+
+    async componentDidMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' })
+    }
+
+    render() {
+        const { hasCameraPermission } = this.state
+
+        if (hasCameraPermission === null) {
+            return <View />
+        }
+        else if (hasCameraPermission === false) {
+            return <Text> No access to camera</Text>
+        }
+        else {
+            return (
+                <View style={{ flex: 1 }}>
+                    <Camera style={styles.container} type={this.state.type} >
+                        <View style={styles.content}>
+                            <MaterialCommunityIcons
+                                onPress={() => {
+                                    alert("PARTY TIME");
+                                }}
+                                name="cannabis" style={{ color: 'white', fontWeight: 'bold', fontSize: 50 }}>
+                            </MaterialCommunityIcons>
+                            <MaterialCommunityIcons
+                                onPress={() => {
+                                    alert("STOP HAMMERTIME!");
+                                }}
+                                name="circle-outline"
+                                style={{ color: 'white', fontSize: 100 }}
+                            ></MaterialCommunityIcons>
+                            <MaterialCommunityIcons
+                                onPress={() => {
+                                    this.setState({
+                                        type: this.state.type === Camera.Constants.Type.back ?
+                                            Camera.Constants.Type.front :
+                                            Camera.Constants.Type.back
+                                    })
+                                }}
+                                name="camera-party-mode" style={{ color: 'white', fontWeight: 'bold', fontSize: 50 }}>
+                            </MaterialCommunityIcons>
+                        </View>
+                    </Camera>
+                </View>
+            )
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        flexDirection: 'column',
+        justifyContent:'flex-end',
+        borderColor:'blue',
+        borderWidth: 10,
+    },
+    content:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginBottom: 25,
+        maxHeight: 120,
+    }
+});
