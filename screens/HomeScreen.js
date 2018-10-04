b1ff8ce4bccdfd485d54a637c92bed60faffefe4
tjabejohannes <tjabejv@stud.ntnu.no>
2018-10-04 22:15:46 +0200

    Merge fix

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,6 +14,7 @@ import AnimateNumber from 'react-native-countup'
 import { StackedAreaChart } from 'react-native-svg-charts'
 import * as shape from 'd3-shape'
 
+
 const styles = createStyles();
 
 
@@ -68,6 +69,7 @@ export default class HomeScreen extends React.Component {
                 { onPress: () => console.log('dates') },]
         };
     }
+
     static navigationOptions = {
         header: null,
     };
